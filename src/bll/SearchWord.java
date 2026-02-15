68adab28d65734700ce31d323e3bdcf5b40918ec
AliHassan-15 <hassanakramali@gmail.com>
2026-02-15 18:06:54 +0500

    fix: Add null safety check in SearchWord.searchKeyword() #14

--- src/bll/SearchWord.java
+++ src/bll/SearchWord.java
@@ -13,9 +13,8 @@ import pl.EditorPO;
 public class SearchWord {
 	public static List<String> searchKeyword(String keyword, List<Documents> docs) {
 		final Logger LOGGER = LogManager.getLogger(EditorPO.class);
-		// TODO Auto-generated method stub
 		List<String> getFiles = new ArrayList<>();
-		if (keyword.length() < 3) {
+		if (keyword == null || keyword.length() < 3) {
 			throw new IllegalArgumentException("Could not Search, Please Enter at least 3 letter to search");
 		}
 
